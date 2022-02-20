43ec0af7350a33e14550165026f3e003d21a5990
NUEVOESPACIO <sebacap3@hotmail.com>
2022-02-20 18:47:04 -0300

    Agregado de Mock y u interace minuto 30:13

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,32 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Resolver Hangar",
+        day: "en cuanto pueda Pl",
+        reminder: false
+
+    },
+    {
+        id: 2,
+        text: "VTV",
+        day: "en cuanto pueda Feb",
+        reminder: false
+
+    },
+    {
+        id: 3,
+        text: "Credencial",
+        day: "en cuanto pueda Nov",
+        reminder: true
+
+    },
+    {
+        id: 4,
+        text: "OJO",
+        day: "en cuanto pueda Jul",
+        reminder: false
+
+    },
+
+]
